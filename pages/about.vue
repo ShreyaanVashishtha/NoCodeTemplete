928a7e1b88539618caee9a19a341479c5854d150
Shreyaan Vashishtha <77964841+ShreyaanVashishtha@users.noreply.github.com>
2021-03-14 15:57:43 +0800

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,27 @@
 <template>
   <main>
+    <Title = "About"/>
     <h1>Home page</h1>
     <NuxtLink to="/about">
       About (internal link that belongs to the Nuxt App)
     </NuxtLink>
     <a href="https://nuxtjs.org">External Link to another page</a>
+    
+    <style lang="scss" scoped>
+@import '../assets/style/variables';
+@media (max-width: 1200px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+  @media (max-width: 960px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  @media (max-width: 320px) {
+    grid-template-columns: 1fr;
+  }
+}
+</style>
   </main>
 </template>
