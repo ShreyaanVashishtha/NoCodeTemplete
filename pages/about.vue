95ca176735b6d2f4abe6d6356546aee69e1728b6
Shreyaan Vashishtha <77964841+ShreyaanVashishtha@users.noreply.github.com>
2021-03-14 21:49:11 +0800

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,24 +1,10 @@
 <template>
   <main>
     <h1>About</h1>
-     <h2>Killed by Google. There's a lot of them! By Shreyaan Vashishtha</h2>
+     <h4>Killed by Google. There's a lot of them! By Shreyaan Vashishtha</h4>
+    <p class="font-italic">
+     KilledByGoogle is an Open-Source Project and can be found on <a href = "https://github.com/ShreyaanVashishtha/NoCodeTemplete"> GitHub.</a>
+    </p>
     <a href="https://nuxtjs.org">External Link to another page</a>
-    
-    <style lang="scss" scoped>
-@import '../assets/style/variables';
-@media (max-width: 1200px) {
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-  }
-  @media (max-width: 960px) {
-    grid-template-columns: 1fr 1fr 1fr;
-  }
-  @media (max-width: 768px) {
-    grid-template-columns: 1fr 1fr;
-  }
-  @media (max-width: 320px) {
-    grid-template-columns: 1fr;
-  }
-}
-</style>
-  </main>
+   </main>
 </template>
