29f908ea71b9fedf1e7872fa4a95146efd19e38e
Shreyaan Vashishtha <77964841+ShreyaanVashishtha@users.noreply.github.com>
2021-03-14 22:16:33 +0800

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,20 @@
 <template>
   <main>
-    <h1>    About</h1>
+   <h1 class="section-title">
+   About
+   </h2>
      <p class="pa-md-4 mx-lg-auto" >    Killed by Google. There's a lot of them! By Shreyaan Vashishtha</p>
     <p class="font-italic">
         KilledByGoogle is an Open-Source Project and can be found on <a href = "https://github.com/ShreyaanVashishtha/NoCodeTemplete"> GitHub.</a>
     </p>
+    
+    <script>
+import TheHeader from '~/components/TheHeader.vue'
+
+export default {
+  name: 'Index',
+  components: {
+    TheHeader
+    },
    </main>
 </template>
