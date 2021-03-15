a1926115cd74bc04eb8943b0323f597dc48deb79
Shreyaan Vashishtha <77964841+ShreyaanVashishtha@users.noreply.github.com>
2021-03-15 10:09:13 +0800

    Create blog.vue

---
+++ pages/blog.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Blog</h1>
+  </div>
+</template>
+<script>
+
+export default {
+
+  created() {
+    window.location.href = "http://www.shreyaan.tech/";
+  },
+
+};
+</script>
