be093d6ca7317be786b50ad1c64bd9bc667f4878
Shreyaan Vashishtha <77964841+ShreyaanVashishtha@users.noreply.github.com>
2021-03-15 21:20:45 +0800

    Create email.vue

---
+++ pages/email.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+   <h1 class="section-title">
+   Email Me
+   </h2>
+     <p class="pa-md-4 mx-lg-auto" >    Killed by Google. There's a lot of them! By Shreyaan Vashishtha</p>
+    <p class="font-italic">
+        Click <a href = "https://github.com/ShreyaanVashishtha/NoCodeTemplete"> here </a> to email me or write an email to killedbygoogle@shreyaan.tech OR shreyaan2007@gmail.com
+    </p>
+    <v-btn
+    color = "primary"
+    depressed
+    elevation = "24"
+    large
+    medium
+    small
+    x-large
+    x-small
+    ><a href = "https://killedbygoogle.info">Home</a></v-btn>
+
+ </template>
