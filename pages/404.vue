f3caa90f00ddbd9581a47cf9c7df3a0342350c9e
Shreyaan Vashishtha <77964841+ShreyaanVashishtha@users.noreply.github.com>
2021-03-16 10:47:06 +0800

    Update 404.vue

--- pages/404.vue
+++ pages/404.vue
@@ -1,8 +1,5 @@
 <template>
   <main>
-   <h1 class="section-title">
-   About
-   </h2>
      <p class="pa-md-4 mx-lg-auto" >    Oh Snap! Error 404, Page Not Found</p>
     <v-btn
     color = "primary"
