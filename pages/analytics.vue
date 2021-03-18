218372926e851bb189018c7419772e25b1db597b
Shreyaan Vashishtha <77964841+ShreyaanVashishtha@users.noreply.github.com>
2021-03-18 15:00:33 +0800

    Create analytics.vue

---
+++ pages/analytics.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+   <h1 class="section-title">
+   Analytics
+   </h2>
+    <p class="font-italic">
+        KilledByGoogle is an Open-Source Project and can be found on <a href = "https://app.netlify.com/sites/killedbygooglesv/analytics">Click Here</a>
+    </p>
+    <v-btn
+    color = "primary"
+    depressed
+    elevation = "24"
+    large
+    medium
+    small
+    x-large
+    x-small
+    ><a href = "https://killedbygoogle.info">Home</a></v-btn>
+
+ </template>
